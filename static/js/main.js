eaf7f923949bb7e93be4ec167d038cf1b198eaaa
Hill-boss <ch124471@umconnect.umt.edu>
2020-01-30 18:46:47 -0700

    Signed-off-by: Hill-boss <ch124471@umconnect.umt.edu>

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,6 @@
-fdvd
+function toggle_index_info(ON, OFF) {
+  ON = document.getElementById(ON);
+  OFF = document.getElementById(OFF);
+  ON.style.display = 'Block';
+  OFF.style.display = 'None';
+}
