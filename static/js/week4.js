148f533488e73dad6009f6e121852f3109059137
Unknown <bosscodycollege@yahoo.com>
2020-02-07 22:35:15 -0700

    update

---
+++ static/js/week4.js
@@ -0,0 +1,17 @@
+function validate() {
+  form = document.forms['loginForm'];
+
+
+  if ((form['firstname'].value == '') || (form['lastname'].value == '')) {
+    alert("Must enter first adn last name.");
+    return false;
+  }
+  if ((isNaN(form['number'].value)) || (form['number'].value == '')) {
+    return false;
+  }
+  if ( !(form['email'].value.includes("@")) || (form['email'].value = "") )  {
+    alert("You must enter a valid email address");
+    return false;
+  }
+  return true;
+}
