5e33fa9da7c700cc97746f7722b543b4127d6880
Hill-Boss <bosscodycollege@yahoo.com>
2020-02-11 17:14:37 -0700

    php

--- Week5/wk5.php
+++ Week5/wk5.php
@@ -1,24 +1,17 @@
 <?php
+$x = 1;
+var_dump($x);
+$y = $z = $x + $x;
+$x = "just no";
+var_dump($x, $y, $z);
+?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- ?>
+<?php
+$even_numbers =[];
+$even_numbers[2] = 0;
+$even_numbers["Melissa"] = 1;
+$even_numbers[0] = 2;
+print_r($even_numbers);
+var_dump($even_numbers);
+var_dump(print_r($even_numbers, true));
+?>
