c8e90f120897d85e7dd3f553190a4ae3e91356b4
Hill-Boss <bosscodycollege@yahoo.com>
2020-02-25 09:43:28 -0700

    fix

--- Week5/Form/index.php
+++ Week5/Form/index.php
@@ -16,13 +16,13 @@
       $postNameArr = array('firstname', 'lastname', 'number', 'email', 'psw', 'psw-re');
 
       // Find all of the post identifiers within $_POST
-      $echo = "<div id=\"form-return-modal-01\" class=\"w3-modal\" style=\"display:block;\" onclick=\"document.getElementById('form-return-modal-01').style.display='none'\">";
-      $echo =$echo."<div class=\"w3-modal-content w3-card-4 w3-teal w3-round-xlarge\">";
-      $echo =$echo."<span onclick=\"document.getElementById('form-return-modal-01').style.display='none'\" class=\"w3-btn w3-display-topright w3-round-xlarge\">&times;</span>";
-      $echo =$echo."<br><br>";
-      $echo =$echo."<div class=\"w3-container w3-white\">";
-      $echo =$echo."<div style=\"width:fit-content;margin-left:25%;\">";
-      $echo =$echo."<h3>";
+      echo "<div id=\"form-return-modal-01\" class=\"w3-modal\" style=\"display:block;\" onclick=\"document.getElementById('form-return-modal-01').style.display='none'\">";
+      echo "<div class=\"w3-modal-content w3-card-4 w3-teal w3-round-xlarge\">";
+      echo "<span onclick=\"document.getElementById('form-return-modal-01').style.display='none'\" class=\"w3-btn w3-display-topright w3-round-xlarge\">&times;</span>";
+      echo "<br><br>";
+      echo "<div class=\"w3-container w3-white\">";
+      echo "<div style=\"width:fit-content;margin-left:25%;\">";
+      echo "<h3>";
 
       foreach ($postNameArr as $postName)
       {
@@ -31,15 +31,15 @@
           switch ($postName)
           {
           case $postNameArr[0]:
-            $echo =$echo."Firstname: ".$_POST[$postNameArr[0]]."<br>";
+            echo "Firstname: ".$_POST[$postNameArr[0]]."<br>";
             break;
 
           case $postNameArr[1]:
-            $echo =$echo."Lastname: ".$_POST[$postNameArr[1]]." <br>";
+            echo "Lastname: ".$_POST[$postNameArr[1]]." <br>";
             break;
 
           case $postNameArr[2]:
-            $echo =$echo."Number: ".$_POST[$postNameArr[2]]." <br>";
+            echo "Number: ".$_POST[$postNameArr[2]]." <br>";
             break;
 
           case $postNameArr[3]:
@@ -47,28 +47,25 @@
             break;
 
           case $postNameArr[4]:
-            $echo =$echo."password: ".$_POST[$postNameArr[4]]." <br>";
+            echo "password: ".$_POST[$postNameArr[4]]." <br>";
             break;
 
           case $postNameArr[5]:
-            $echo =$echo."confirmed: ".$_POST[$postNameArr[5]]." <br>";
+            echo "confirmed: ".$_POST[$postNameArr[5]]." <br>";
             break;
 
           default:
-            $echo =$echo.$postName." variable not known";
+            echo $postName." variable not known";
           }
         } else {
-          $echo =$echo."Post was empty";
+          echo "Post was empty";
         }
       }
-      $echo =$echo."</h3></div></div>";
-      $echo =$echo."<footer class=\"w3-container w3-teal w3-round-xlarge\" style=\"height:40px;\"></footer>";
-      $echo =$echo."</div></div>";
-      echo ''.$echo;
+      echo "</h3></div></div>";
+      echo "<footer class=\"w3-container w3-teal w3-round-xlarge\" style=\"height:40px;\"></footer>";
+      echo "</div></div>";
     }
   ?>
-
-
 </body>
 
 </html>
