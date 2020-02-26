990e333400dd2ba291209f27e475f3027222edb8
Hill-Boss <bosscodycollege@yahoo.com>
2020-02-26 11:08:30 -0700

    fpdf

--- Week5/index.php
+++ Week5/index.php
@@ -220,5 +220,70 @@
         </form>
       </div>
     </div>
+    <br>
+    <h4>Part 2 code</h4><br>
+    <p>
+      this is the code that formats the form information and displays it on /Week5/Form/
+    </p>
+
+    <pre><code>
+    &lt;?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $postNameArr = array('firstname', 'lastname', 'number', 'email', 'psw', 'psw-re');
+
+        // Find all of the post identifiers within $_POST
+        echo "<div class=\"w3-modal\" style=\"display:block;\" onclick=\"gotoPage('../')\">";
+        echo "<div class=\"w3-modal-content w3-card-4 w3-teal w3-round-xlarge\">";
+        echo "<span onclick=\"gotoPage('../')\" class=\"w3-btn w3-display-topright w3-round-xlarge\">&times;</span>";
+        echo "<br><br>";
+        echo "<div class=\"w3-container w3-white\">";
+        echo "<div style=\"width:fit-content;margin-left:25%;\">";
+        echo "<h3>";
+
+        foreach ($postNameArr as $postName)
+        {
+          if (array_key_exists($postName, $_POST))
+          {
+            switch ($postName)
+            {
+            case $postNameArr[0]:
+              echo "Firstname: ".$_POST[$postNameArr[0]]."<br>";
+              break;
+
+            case $postNameArr[1]:
+              echo "Lastname: ".$_POST[$postNameArr[1]]." <br>";
+              break;
+
+            case $postNameArr[2]:
+              echo "Number: ".$_POST[$postNameArr[2]]." <br>";
+              break;
+
+            case $postNameArr[3]:
+              echo "email: ".$_POST[$postNameArr[3]]." <br>";
+              break;
+
+            case $postNameArr[4]:
+              echo "password: ".$_POST[$postNameArr[4]]." <br>";
+              break;
+
+            case $postNameArr[5]:
+              echo "confirmed: ".$_POST[$postNameArr[5]]." <br>";
+              break;
+
+            default:
+              echo $postName." variable not known";
+            }
+          } else {
+            echo "Post was empty";
+          }
+        }
+        echo "</h3></div></div>";
+        echo "<footer class=\"w3-container w3-teal w3-round-xlarge\" style=\"height:40px;\"></footer>";
+        echo "</div></div>";
+      }
+    ?>
+    </code></pre>
+
   </body>
 </html>
