0e6d58fd52b33d0808802628d772f70e2c4ec970
Hill-Boss <bosscodycollege@yahoo.com>
2020-03-04 11:31:22 -0700

    Ecommerce midterm start

---
+++ Week6/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Week 6</title>
+  </head>
+  <body>
+    <h3>Idk how to do the pdf stuff</h3>
+    <p></p>
+  </body>
+</html>
---
+++ Week8/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../static/css/main.css">
+    <link rel="stylesheet" href="../static/css/w3.css">
+
+    <title>Ecommerce Midterm Site</title>
+  </head>
+  <body>
+    <div class="w3-top w3-bottom w3-light-gray" style="overflow-y:scroll">
+      <div class="w3-top w3-left w3-right w3-teal">
+        <div class="w3-third">
+          <img src="./imgs/logo.png" alt="Logo Here">
+        </div>
+        <div class="w3-third w3-center">
+          <h4>Ecommerce</h4>
+        </div>
+        <div class="w3-third w3-center">
+          <h6>Login</h6>
+        </div>
+
+      </div>
+    </div>
+  </body>
+</html>
