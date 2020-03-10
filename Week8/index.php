cf2b02f649779ca21f7853023406ab0d876e4d49
Hill-Boss <bosscodycollege@yahoo.com>
2020-03-10 09:05:29 -0600

    Week8

--- Week8/index.php
+++ Week8/index.php
@@ -22,5 +22,41 @@
 
       </div>
     </div>
+  <?php
+    // connect to database
+  	$username = "admin";
+  	$password = "pass123";
+
+    $conn = mysqli_connect("webdev.cs.umt.edu","ch124471", "","ch124471")
+       or die('Could not connect');
+    echo "Connected successfully <br>";
+
+    $query = "SELECT * from auth";
+    // Try changing the SQL to search for username and password.
+    //Modify the line below.  Note the concatenation of the variable and single quotes
+
+    //$query = "SELECT * from auth where userID = '" . $username . "'";
+    $result = mysqli_query($conn, $query)
+      or die("Query failed ");
+    echo "query ok\n";
+    $num_results = mysqli_num_rows($result);
+    echo '<p>Number of rows found: '.$num_results.'</p>';
+    //if numresults = 1 then user has authenticated
+    //modify the block of code below
+
+    echo " <table border='1'>\n";
+    while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+  	   echo "\t<tr>\n";
+  	   foreach ($line as $col_value) {
+  	      echo "\t\t<td>$col_value</td>\n";
+  	   }
+  	   echo "\t</tr>\n";
+    }
+    echo "</table>\n";
+    //Free result set
+    mysqli_free_result($result);
+    //close connection
+    mysqli_close($conn);
+  ?>
   </body>
 </html>
